fea0c5ec8b2f21e3a4ddfd95c040cfad59d22e62
jayashridangat <jayashridangat0@gmail.com>
2023-10-01 20:05:13 +0530

    &apos;

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,14 @@ export default function index() {
       </head>
       <div className="mx-2">
      <p >Hi!</p>
-     <p >I&aposm Jayashri!</p>
+     <p >I&apos;m Jayashri!</p>
 <p className="my-1">This is all about me-</p>
 <br></br>
 An aspiring to be a software development engineer
 <p>Pursuing Engineering in Instrumentation and Control</p>
 <p>Currently in final year final year - COEP Technological University</p>
 <br></br>
-<p>That&aposs all....</p>
+<p>That&apos;s all....</p>
 <br></br>
 Thank You!
     </div>
